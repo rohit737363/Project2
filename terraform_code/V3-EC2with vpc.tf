1fe5f423a34165fe644a3e3753b6e3887520f643
rohit737363 <chougalerohit73@gmail.com>
2023-09-04 19:01:26 +0530

    ec2with vpc

---
+++ terraform_code/V3-EC2with vpc.tf
@@ -0,0 +1,102 @@
+provider "aws" {
+    region ="us-east-1"
+}
+
+resource "aws_instance" "demo-server" {
+    ami = "ami-053b0d53c279acc90"
+    instance_type = "t2.micro"
+    key_name = "newkey"
+    //security_groups = ["demo-sg"]
+    vpc_security_group_ids = [ aws_security_group.demo-sg.id ]
+    subnet_id = aws_subnet.dpw-public_subent_01.id
+
+    
+  
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  vpc_id = aws_vpc.dpw-vpc.id
+
+  
+
+  ingress {
+    description      = "Shh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-port"
+  }
+}
+
+  resource "aws_vpc" "dpw-vpc" {
+       cidr_block = "10.0.0.0/16"
+       tags = {
+        Name = "dpw-vpc"
+     }
+   }
+   
+resource "aws_subnet" "dpw-public_subent_01" {
+    vpc_id = aws_vpc.dpw-vpc.id
+    cidr_block = "10.1.1.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1a"
+    tags = {
+      Name = "dpw-public_subent_01"
+    }
+}
+
+resource "aws_subnet" "dpw-public_subent_02" {
+    vpc_id = aws_vpc.dpw-vpc.id
+    cidr_block = "10.1.2.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1a"
+    tags = {
+      Name = "dpw-public_subent_02"
+    }
+}
+
+
+resource "aws_internet_gateway" "dpw-igw" {
+    vpc_id = aws_vpc.dpw-vpc.id
+    tags = {
+      Name = "dpw-igw"
+    }
+}
+
+
+resource "aws_route_table" "dpw-public-rt" {
+    vpc_id = aws_vpc.dpw-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.dpw-igw.id
+    }
+    tags = {
+      Name = "dpw-public-rt"
+    }
+}
+
+
+resource "aws_route_table_association" "dpw-rta-public-subent-01" {
+    subnet_id = aws_subnet.dpw-public_subent_01.id
+    route_table_id = aws_route_table.dpw-public-rt.id
+}
+
+resource "aws_route_table_association" "dpw-rta-public-subent-02" {
+    subnet_id = aws_subnet.dpw-public_subent_02.id
+    route_table_id = aws_route_table.dpw-public-rt.id
+}
